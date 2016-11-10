d120b0ffc4314e3295888b38973a277f373c3217
Rahul R <rahulpisharody@gmail.com>
2016-11-10 11:38:43 +0530

    Added support for finding .blend file

--- AthenaRender.py
+++ AthenaRender.py
@@ -8,7 +8,6 @@ current_dir = os.getcwd()
 
 #Find the Blender executable
 list_dir = [d for d in os.listdir('.') if ((os.path.isdir(d)) and (not d.startswith('.')))]
-print (list_dir)
 
 for directory in list_dir:
     if ('blender' in os.listdir(directory)):
@@ -26,10 +25,24 @@ except:
     print('Usage: python render.py <name> <anim (0/1)>')
     sys.exit()
 
+#Find .blend File
+list_dir = [d for d in os.listdir('.') if (not d.startswith('.'))]
+if (name in list_dir):
+    to_render = name
+else:
+    list_dir = [d for d in list_dir if (os.path.isdir(d))]
+    for directory in list_dir:
+        if (name in os.listdir(directory)):
+            to_render = directory+'/'+name
+            break
+    else:
+        print(name + ' not found')
+        sys.exit()
+
 #Set Cycles Sampling
 try:
     sampling = str(sys.argv[3])
 except:
     sampling = str(300)
 
-os.system(to_dir+'/blender -b \"' +name+'\" -o //renders/'+os.path.basename(os.path.splitext(name)[0])+' -P RenderSettings.py -- '+anim+' '+sampling)
+os.system(to_dir+'/blender -b \"' +to_render+'\" -o //renders/'+name+' -P RenderSettings.py -- '+anim+' '+sampling)
